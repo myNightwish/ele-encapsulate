87c3dcdedd871c00671571bc58ca35593da81091
zhoumaoling <zhoumaoling@baidu.com>
2024-08-18 14:23:41 +0800

    修复样式

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index.js';
-// import encapsulteComps from './packages/index.js';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import './styles/ui.scss';
 
+// import encapsulteComps from '../packages/index';
 // 1、注意这里的引入文件后缀不要带mjs
 // 2、 ui.scss是全局项目文件需要被引入
 // import encapsulteComps from '../lib/index';
 import encapsulteComps from 'mi-encapsulate-components/index';
-import 'mi-encapsulate-components/style.css';
+// import 'mi-encapsulate-components/style.css';
 
 // 按需引入
 // todo：为什这里又要带umd了？？
 // import MiCity from 'mi-encapsulate-components/MiCity/index.umd.js';
 
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
 const app = createApp(App);
 app.use(router).use(ElementPlus)
 .use(encapsulteComps)
